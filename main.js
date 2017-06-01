df42b1096f6a15820efac8721112d7c007d5fb4e
Vas2016 <vasily.yuryev.a1@gmail.com>
2017-06-01 11:51:18 +0700

    Fixed

--- main.js
+++ main.js
@@ -9,15 +9,7 @@ modulesTcp.start();
 //searching(udp multi)
 require("./modules_files/searchModulesNet.js").searchModulesNet();
 
-//express http server
 
-// setTimeout(function () {
-//     function handleInterval() {
-//         gv.modules["192.168.0.61"].handleIntervalM();
-//     }
-
-//     setInterval(handleInterval, 30000);
-// }, 2000);
 var expressServer = require("./expressServer");
 
 expressServer.startExpress(8080); 
